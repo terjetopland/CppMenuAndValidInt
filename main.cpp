784889ba5bd8f700e77d46310fc8f1ae29feafaf
Terje Topland <74875770+terjetopland@users.noreply.github.com>
2022-03-28 12:52:58 +0200

    Added comments in 'main'

--- main.cpp
+++ main.cpp
@@ -4,8 +4,10 @@
 
 int main() {
 
-
+    //Takes whatever strings u want to display and displays it to the console
     MMenu({"This is the first menu", "This is the second", "This is the third option", "Fourth option, could be e.g. 'Exit'"});
+
+    //Ask the user to choose one of the valid numbers up 'max_valid_int', and then validates it.
     ReadUserInput(4);
 
     return 0;
